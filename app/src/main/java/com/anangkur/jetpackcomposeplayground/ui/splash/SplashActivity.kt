7336d0bae7fa90d8c9dffb11de934dffdf762a50
Anang Kurniawan <anangk97@gmail.com>
2020-07-15 09:46:13 +0700

    build(app): fixing layout for SplashActivity.

--- app/src/main/java/com/anangkur/jetpackcomposeplayground/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/anangkur/jetpackcomposeplayground/ui/splash/SplashActivity.kt
@@ -1,8 +1,12 @@
 package com.anangkur.jetpackcomposeplayground.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.ui.core.setContent
+import androidx.ui.graphics.imageFromResource
+import androidx.ui.res.vectorResource
+import com.anangkur.jetpackcomposeplayground.R
 
 class SplashActivity: AppCompatActivity() {
 
@@ -10,7 +14,16 @@ class SplashActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            splashScreenContent()
+            splashScreenContent(
+                appTitle = getString(R.string.app_name),
+                imageAsset = vectorResource(R.drawable.ic_baseline_adb_24)
+            )
         }
+
+        val handler = Handler()
+        handler.postDelayed({
+
+            finish()
+        }, 1000)
     }
 }
